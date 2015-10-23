a1ebed0cb3c581aa74fecefab8c1efeb56105c8d
Luc-Olivier Boulet <lucolivierboulet@gmail.com>
2015-10-23 17:04:02 -0400

    Add i18n

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,3 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend();
+export default Ember.Controller.extend({
+  // NOTE: The line below is optional. You can use this line if you need translations into your controller.
+  // i18n: Ember.inject.service()
+});
