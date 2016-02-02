2b8e9807b98d08560324fef816c1cfb645a60c89
Simon Prévost <sprevost@mirego.com>
2016-02-02 13:43:21 -0500

    Update to ember 2.3.0beta1 and ember-data 2.3.0

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
+import Resolver from './resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-let {modulePrefix, podModulePrefix} = config;
-let App = Ember.Application.extend({modulePrefix, podModulePrefix, Resolver});
+const {modulePrefix, podModulePrefix} = config;
+const App = Ember.Application.extend({
+  modulePrefix,
+  podModulePrefix,
+  Resolver
+});
 
 loadInitializers(App, modulePrefix);
 
