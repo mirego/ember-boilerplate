6922606a789707d291077447ddfc60ed78b1a085
Rémi Prévost <remi@exomel.com>
2017-06-09 15:09:26 -0400

    Remove useless Raven code from application route

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,10 +1,3 @@
 import Ember from 'ember';
-import Raven from 'npm:raven-js';
-import config from 'ember-boilerplate/config/environment';
 
-export default Ember.Route.extend({
-  beforeModel() {
-    if (!config.SENTRY.DSN) return;
-    Raven.config(config.SENTRY.DSN).install();
-  }
-});
+export default Ember.Route.extend();
