d5b5cf43ef34996fe409714788d381f270625440
Charles Demers <charles.demers6@gmail.com>
2018-01-26 15:25:31 -0500

    Parallelize tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,7 @@
 import resolver from './helpers/resolver';
 import {setResolver} from 'ember-mocha';
+import loadEmberExam from 'ember-exam/test-support/load';
+
+loadEmberExam();
 
 setResolver(resolver);
