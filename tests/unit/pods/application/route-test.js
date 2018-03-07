a438937db0b220eb95fcd34493268bf7f43345c5
dfillion <dfillion@mirego.com>
2018-03-07 11:17:21 -0500

    Add basic application route
    This route must be present in every projet, and it also makes sure
    the boilerplate now includes the `pods` folder.

---
+++ tests/unit/pods/application/route-test.js
@@ -0,0 +1,16 @@
+import {expect} from 'chai';
+import {describe, it} from 'mocha';
+import {setupTest} from 'ember-mocha';
+
+describe('Application route', () => {
+  setupTest('route:application', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  it('exists', function() {
+    const route = this.subject();
+
+    expect(route).to.be.ok;
+  });
+});
