904a42cd9235a0d7f7277aed96bee0f12088c8fb
Charles Demers <charles.demers6@gmail.com>
2018-12-20 12:11:50 -0500

    Update dependencies and get rid of custom test libraries

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,6 +2,10 @@ import Application from '../app';
 import config from '../config/environment';
 import {setApplication} from '@ember/test-helpers';
 import loadEmberExam from 'ember-exam/test-support/load';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
 
 loadEmberExam();
 
