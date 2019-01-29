f8b419860985d6db41bd60c744d5aff74bd94c73
Rémi Prévost <remi@exomel.com>
2019-01-29 08:29:23 -0500

    Add project README.md

--- project-renamer.sh
+++ project-renamer.sh
@@ -2,6 +2,11 @@
 
 error_status=0
 
+if [[ -z "$1" ]] ; then
+  echo 'You must specify your project name in kebab-case as first argument.'
+  exit 0
+fi
+
 RED='\033[0;31m'
 RED_BOLD='\033[1;31m'
 GREEN='\033[0;32m'
@@ -9,10 +14,8 @@ GREEN_BOLD='\033[1;32m'
 YELLOW='\033[0;33m'
 NO_COLOR='\033[0m'
 
-OLD_REPO_NAME=ember-boilerplate
-OLD_PACKAGE_NAME=ember-boilerplate
-NEW_REPO_NAME=client-project-webapp
-NEW_PACKAGE_NAME=short-project-name
+OLD_NAME=ember-boilerplate
+NEW_NAME=$1
 
 header() {
   echo "\n\n${YELLOW}▶ $1${NO_COLOR}"
@@ -24,13 +27,19 @@ if [[ ! -x "/usr/bin/find" || ! -x "/usr/bin/sed" ]]; then
 fi
 
 header "📦  Replacing repo name in package.json"
-/usr/bin/sed -i '' "s/https:\/\/github.com\/mirego\/$OLD_REPO_NAME/https:\/\/github.com\/mirego\/$NEW_REPO_NAME/g" package.json &&
+/usr/bin/sed -i '' "s/https:\/\/github.com\/mirego\/$OLD_NAME/https:\/\/github.com\/mirego\/$NEW_NAME/g" package.json &&
 
 echo "${GREEN}↳ Done ✔${NO_COLOR}"
 
 header "🔍  Replacing package name in .js, .ts, .json and .html files"
 for file in `/usr/bin/find . \( -type f -name "*.js" -or -name "*.ts" -or -name "*.html" -or -name "*.json" \) -and ! -path "./node_modules/*"`; do
-  /usr/bin/sed -i '' "s/$OLD_PACKAGE_NAME/$NEW_PACKAGE_NAME/g" $file
+  /usr/bin/sed -i '' "s/$OLD_NAME/$NEW_NAME/g" $file
 done
+
+echo "${GREEN}↳ Done ✔${NO_COLOR}"
+
+header "🗄  Renaming README.md and BOILERPLATE_README.md files"
+rm -fr README.md
+mv BOILERPLATE_README.md README.md
 
 echo "${GREEN}↳ Done ✔${NO_COLOR}"
