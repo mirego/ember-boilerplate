e9dd1b0ed69303870a317a3274473f7742a03114
Rémi Prévost <remi@exomel.com>
2019-07-15 09:03:04 -0400

    Add “slashes” server middleware

---
+++ node-server/middleware/deduplicateSlashes.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+'use strict';
+
+// Vendor
+const url = require('url');
+
+// Constants
+const PERMANENT_REDIRECT_STATUS_CODE = 301;
+
+module.exports = (request, response, next) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (!parsedUrl.pathname.match(/\/{2,}/)) return next();
+
+  response.redirect(
+    PERMANENT_REDIRECT_STATUS_CODE,
+    parsedUrl.pathname.replace(/\/{2,}/g, '/')
+  );
+};
