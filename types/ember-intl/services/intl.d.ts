e5bbd007d116319b693949e02fb32c72b8fa1676
Pascal Boutin <pboutin@mirego.com>
2019-07-31 21:50:32 -0400

    Make intl translate optional params really optional

--- types/ember-intl/services/intl.d.ts
+++ types/ember-intl/services/intl.d.ts
@@ -3,7 +3,7 @@ import IntlService from 'ember-intl/services/intl';
 declare module 'ember-intl/services/intl' {
   export default class IntlService {
     locales: string[];
-    t(translationKey: string, optionalOptions: object, optionalFormats: object): string;
+    t(translationKey: string, optionalOptions?: object, optionalFormats?: object): string;
     exists(translationKey: string, localeName: string): boolean;
     setLocale(localeName: string): void;
     addTranslations(localeName: string, translations: object): object;
