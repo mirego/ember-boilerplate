5907fdd0363951d4f9fb497b09113329603bde66
Charles Demers <cdemers@mirego.com>
2019-09-16 09:00:19 -0400

    Fix middleware URL parsing (#29)

--- node-server/middleware/strip-trailing-slashes.js
+++ node-server/middleware/strip-trailing-slashes.js
@@ -5,9 +5,13 @@ const url = require('url');
 
 // Constants
 const PERMANENT_REDIRECT_STATUS_CODE = 301;
+const canonicalHost = process.env.CANONICAL_HOST;
 
 module.exports = (request, response, next) => {
-  const parsedUrl = new URL(request.url);
+  const parsedUrl = new URL(
+    request.url,
+    `${request.protocol}://${canonicalHost}`
+  );
 
   if (!parsedUrl.pathname.match(/^.+\/$/)) return next();
 
