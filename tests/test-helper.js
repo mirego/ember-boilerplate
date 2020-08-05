2c0de353f9605a5ec5ef7da876892e4a28f9fabe
Pascal Boutin <pboutin@mirego.com>
2020-08-05 19:04:46 -0400

    Init chai dom (#54)
    * Connect chai-dom to the test-helper
    
    * Add chai-dom typings

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,8 +4,10 @@ import {setApplication} from '@ember/test-helpers';
 import start from 'ember-exam/test-support/start';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import chaiDom from 'chai-dom';
 
 chai.use(sinonChai);
+chai.use(chaiDom);
 
 setApplication(Application.create(config.APP));
 
