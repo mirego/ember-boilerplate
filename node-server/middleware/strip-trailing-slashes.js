675a547cfdfcb5dba89e2de8a23ce9ee41c78b1c
Charles Demers <charles.demers6@gmail.com>
2021-04-03 18:25:29 -0400

    Refactor FastBoot server

--- node-server/middleware/strip-trailing-slashes.js
+++ node-server/middleware/strip-trailing-slashes.js
@@ -7,10 +7,14 @@ const url = require('url');
 const PERMANENT_REDIRECT_STATUS_CODE = 301;
 const canonicalHost = process.env.CANONICAL_HOST;
 
+const TRAILING_SLASH_REGEXP = /\/+$/;
+
 module.exports = (request, response, next) => {
   const parsedUrl = new URL(request.url, `${request.protocol}://${canonicalHost}`);
 
-  if (!parsedUrl.pathname.match(/^.+\/$/)) return next();
+  if (!TRAILING_SLASH_REGEXP.test(parsedUrl.pathname)) return next();
 
-  response.redirect(PERMANENT_REDIRECT_STATUS_CODE, parsedUrl.pathname.replace(/\/+$/, ''));
+  parsedUrl.pathname = parsedUrl.pathname.replace(TRAILING_SLASH_REGEXP, '');
+
+  response.redirect(PERMANENT_REDIRECT_STATUS_CODE, parsedUrl.pathname + parsedUrl.search + parsedUrl.hash);
 };
--- node-server/server.js
+++ node-server/server.js
@@ -10,9 +10,6 @@ const forceSSL = require('express-force-ssl');
 const {forceDomain} = require('forcedomain');
 const cacheControl = require('express-cache-controller');
 
-// Custom HTTP Server
-const HTTPServer = require('./express-http-server');
-
 // Middleware
 const internalServerErrorMiddleware = require('./middleware/internal-server-error');
 const stripTrailingSlashes = require('./middleware/strip-trailing-slashes');
@@ -22,20 +19,120 @@ const deduplicateSlashes = require('./middleware/deduplicate-slashes');
 const {asBoolean, asInteger, isPresent} = require('../config/utils');
 const appPackage = require('../package.json');
 
-// Constants
-const OK_HTTP_CODE = 200;
-const FORCE_SSL_OPTIONS = {trustXFPHeader: true, httpsPort: 443};
-const ASSETS_MAX_AGE = 2592000; // 1 month
-const BASIC_AUTH_OPTIONS = {
-  users: {
-    [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD,
-  },
-  challenge: true,
+const setupSentry = app => {
+  if (!isPresent(process.env.SENTRY_DSN)) return;
+
+  Sentry.init(config(process.env.NODE_ENV).sentry);
+
+  app.use(Sentry.Handlers.requestHandler());
 };
 
-// Server
-const httpServer = new HTTPServer({
+const setupHealthCheck = app => {
+  app.use(
+    '/health',
+    cacheControl({
+      noCache: true,
+      private: true,
+      noStore: true,
+      mustRevalidate: true,
+    })
+  );
+
+  app.get('/health', (_, response) => {
+    response.status(200).send({status: 'OK', version: appPackage.version});
+  });
+};
+
+const setupCanonicalHost = app => {
+  if (!isPresent(process.env.CANONICAL_HOST)) return;
+
+  app.use(forceDomain({hostname: process.env.CANONICAL_HOST}));
+};
+
+const setupForceSSL = app => {
+  if (!asBoolean(process.env.FORCE_SSL)) return;
+
+  app.set('forceSSLOptions', {trustXFPHeader: true, httpsPort: 443});
+  app.use(forceSSL);
+};
+
+const setupBasicAuthentication = app => {
+  if (!process.env.BASIC_AUTH_USERNAME || !process.env.BASIC_AUTH_PASSWORD) return;
+
+  app.use(
+    basicAuth({
+      users: {
+        [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD,
+      },
+      challenge: true,
+    })
+  );
+};
+
+const setupURLCorrection = app => {
+  app.use(deduplicateSlashes);
+  app.use(stripTrailingSlashes);
+};
+
+const setupCompression = app => {
+  app.use(compression());
+};
+
+const setupCacheHeaders = app => {
+  if (process.env.PAGES_CACHE_DURATION_IN_SECONDS) {
+    app.use(
+      cacheControl({
+        maxAge: asInteger(process.env.PAGES_CACHE_DURATION_IN_SECONDS),
+      })
+    );
+  } else {
+    app.use(cacheControl({noCache: true}));
+  }
+
+  app.use(
+    ['/robots.txt', '/sitemap.xml', '/sw.js'],
+    cacheControl({
+      noCache: true,
+      private: true,
+      noStore: true,
+      mustRevalidate: true,
+    })
+  );
+
+  app.use(
+    '/assets/*',
+    cacheControl({
+      maxAge: 2592000, // 1 month in seconds,
+    })
+  );
+};
+
+const fastbootServer = new FastBootAppServer({
+  distPath: 'dist',
+  workerCount: asInteger(process.env.WORKER_COUNT),
+  chunkedResponse: true,
+
+  beforeMiddleware(app) {
+    setupSentry(app);
+    setupHealthCheck(app);
+    setupCanonicalHost(app);
+    setupForceSSL(app);
+    setupBasicAuthentication(app);
+    setupURLCorrection(app);
+    setupCompression(app);
+    setupCacheHeaders(app);
+  },
+
   afterMiddleware(app) {
+    // Never cache error responses
+    app.use(function (req, res, next) {
+      if (res.status >= 400) {
+        res.cacheControl = {
+          noCache: true,
+        };
+      }
+    });
+
     if (isPresent(process.env.SENTRY_DSN)) {
       app.use(Sentry.Handlers.errorHandler());
     }
@@ -44,87 +141,4 @@ const httpServer = new HTTPServer({
   },
 });
 
-const app = httpServer.app;
-
-// Sentry
-if (isPresent(process.env.SENTRY_DSN)) {
-  Sentry.init(config(process.env.NODE_ENV).sentry);
-
-  app.use(Sentry.Handlers.requestHandler());
-}
-
-// Health check
-app.use(
-  '/health',
-  cacheControl({
-    noCache: true,
-    private: true,
-    noStore: true,
-    mustRevalidate: true,
-  })
-);
-
-app.get('/health', (_, response) => {
-  response.status(OK_HTTP_CODE).send({status: 'OK', version: appPackage.version});
-});
-
-// Canonical host
-if (process.env.CANONICAL_HOST) {
-  app.use(forceDomain({hostname: process.env.CANONICAL_HOST}));
-}
-
-// Force SSL
-if (asBoolean(process.env.FORCE_SSL)) {
-  app.set('forceSSLOptions', FORCE_SSL_OPTIONS);
-  app.use(forceSSL);
-}
-
-// Basic Authentication
-if (process.env.BASIC_AUTH_USERNAME && process.env.BASIC_AUTH_PASSWORD) {
-  app.use(basicAuth(BASIC_AUTH_OPTIONS));
-}
-
-// Remove a few slashes
-app.use(stripTrailingSlashes);
-app.use(deduplicateSlashes);
-
-// Gzip
-app.use(compression());
-
-// Cache headers
-if (process.env.PAGES_CACHE_DURATION_IN_SECONDS) {
-  app.use(
-    cacheControl({
-      maxAge: asInteger(process.env.PAGES_CACHE_DURATION_IN_SECONDS),
-    })
-  );
-} else {
-  app.use(cacheControl({noCache: true}));
-}
-
-app.use(
-  ['/robots.txt', '/sitemap.xml', '/sw.js'],
-  cacheControl({
-    noCache: true,
-    private: true,
-    noStore: true,
-    mustRevalidate: true,
-  })
-);
-
-// Assets
-app.use(
-  '/assets/*',
-  cacheControl({
-    maxAge: ASSETS_MAX_AGE,
-  })
-);
-
-const server = new FastBootAppServer({
-  distPath: 'dist',
-  workerCount: asInteger(process.env.WORKER_COUNT),
-  httpServer,
-  chunkedResponse: true,
-});
-
-server.start();
+fastbootServer.start();
