dd713ffa02fe1064ce62e8b27e67409dd3c2cf55
Charles Demers <cdemers@mirego.com>
2021-04-06 08:07:59 -0400

    Refactor FastBoot server (#69)

--- node-server/middleware/deduplicate-slashes.js
+++ node-server/middleware/deduplicate-slashes.js
@@ -7,10 +7,14 @@ const url = require('url');
 const PERMANENT_REDIRECT_STATUS_CODE = 301;
 const canonicalHost = process.env.CANONICAL_HOST;
 
+const MULTIPLE_SLASHES_REGEXP = /\/{2,}/g;
+
 module.exports = (request, response, next) => {
   const parsedUrl = new URL(request.url, `${request.protocol}://${canonicalHost}`);
 
-  if (!parsedUrl.pathname.match(/\/{2,}/)) return next();
+  if (!MULTIPLE_SLASHES_REGEXP.test(parsedUrl.pathname)) return next();
 
-  response.redirect(PERMANENT_REDIRECT_STATUS_CODE, parsedUrl.pathname.replace(/\/{2,}/g, '/'));
+  parsedUrl.pathname = parsedUrl.pathname.replace(MULTIPLE_SLASHES_REGEXP, '/');
+
+  response.redirect(PERMANENT_REDIRECT_STATUS_CODE, parsedUrl.pathname + parsedUrl.search + parsedUrl.hash);
 };
