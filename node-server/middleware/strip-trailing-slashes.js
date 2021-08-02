4d90d99057693719256c0fd3298339158788f3fd
Charles Demers <cdemers@mirego.com>
2021-08-02 15:11:28 -0400

    Fix strip-slashes middleware (#75)

--- node-server/middleware/strip-trailing-slashes.js
+++ node-server/middleware/strip-trailing-slashes.js
@@ -7,12 +7,13 @@ const url = require('url');
 const PERMANENT_REDIRECT_STATUS_CODE = 301;
 const canonicalHost = process.env.CANONICAL_HOST;
 
+const NON_ROOT_TRAILING_SLASH_REGEXP = /^.+\/$/;
 const TRAILING_SLASH_REGEXP = /\/+$/;
 
 module.exports = (request, response, next) => {
   const parsedUrl = new URL(request.url, `${request.protocol}://${canonicalHost}`);
 
-  if (!TRAILING_SLASH_REGEXP.test(parsedUrl.pathname)) return next();
+  if (!NON_ROOT_TRAILING_SLASH_REGEXP.test(parsedUrl.pathname)) return next();
 
   parsedUrl.pathname = parsedUrl.pathname.replace(TRAILING_SLASH_REGEXP, '');
 
