39739cca56b86f1f018b1f1b4151cfe01bb4ee5f
Rémi Prévost <remi@exomel.com>
2022-08-01 22:03:23 -0400

    Make config/runtime-environment more readable

--- config/runtime-environment.js
+++ config/runtime-environment.js
@@ -4,19 +4,15 @@ const variables = {
   HELLO_WORLD: process.env.HELLO_WORLD,
 };
 
-const htmlPattern = /<!-- RUNTIME_ENVIRONMENT -->[\s\S]+<!-- \/RUNTIME_ENVIRONMENT -->/m;
-
 const html = `
   <!-- RUNTIME_ENVIRONMENT -->
   <script data-fastboot-ignore="">window.RUNTIME_ENVIRONMENT = ${JSON.stringify(variables)}</script>
   <!-- \/RUNTIME_ENVIRONMENT -->
 `;
 
+const htmlPattern = /<!-- RUNTIME_ENVIRONMENT -->[\s\S]+<!-- \/RUNTIME_ENVIRONMENT -->/m;
+
 const fastBootBuildSandboxGlobals = defaultGlobals =>
   Object.assign({}, defaultGlobals, {RUNTIME_ENVIRONMENT: variables});
 
-module.exports = {
-  htmlPattern,
-  html,
-  fastBootBuildSandboxGlobals,
-};
+module.exports = {html, htmlPattern, fastBootBuildSandboxGlobals};
