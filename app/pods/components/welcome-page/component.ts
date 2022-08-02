a1f7fc164ff965de76f28d3784ba078f1727dc0f
Rémi Prévost <remi@exomel.com>
2022-08-01 21:05:13 -0400

    Use “env”

--- app/pods/components/welcome-page/component.ts
+++ app/pods/components/welcome-page/component.ts
@@ -1,10 +1,10 @@
 // Vendor
 import Component from '@glimmer/component';
 
-import runtime from 'ember-boilerplate/utils/runtime-environment-variable';
+import env from 'ember-boilerplate/utils/runtime-environment';
 
 export default class WelcomePage extends Component {
   get helloWorld() {
-    return runtime('HELLO_WORLD');
+    return env('HELLO_WORLD');
   }
 }
