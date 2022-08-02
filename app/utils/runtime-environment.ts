d7e0465045b6e4220fdadc039b9f21bf5a995746
Rémi Prévost <remi@exomel.com>
2022-08-02 09:29:57 -0400

    Use chaining

--- app/utils/runtime-environment.ts
+++ app/utils/runtime-environment.ts
@@ -1,6 +1,6 @@
 // NOTE: This is where we figure out if we use the client-side or server-side runtime environment
 export default (key: string) => {
-  if (typeof window !== 'undefined' && window.RUNTIME_ENVIRONMENT) {
+  if (window?.RUNTIME_ENVIRONMENT) {
     return window.RUNTIME_ENVIRONMENT[key];
   } else {
     return RUNTIME_ENVIRONMENT[key];
